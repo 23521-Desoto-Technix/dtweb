b2bfd716d21228df662694437baaec0e4e7eda12
PythonAtSea <HudsonGreen27@gmail.com>
2024-11-09 17:18:11 -0600

    members

--- app/components/Members.tsx
+++ app/components/Members.tsx
@@ -1,9 +1,14 @@
 
 const members = [
-    { name: 'Nathan', role: 'Captain', bio: "Lorem ipsum doler sait" },
-    { name: 'Hudson', role: 'Software + CAD', bio: "Lorem ipsum doler sait" },
-    { name: 'Nathan', role: 'Captain', bio: "Lorem ipsum doler sait" },
-    { name: 'Hudson', role: 'Software + CAD', bio: "Lorem ipsum doler sait" },
+    { name: 'Nathan', role: 'Captain', bio: "Lorem ipsum doler sait", mentor: false},
+    { name: 'Hudson', role: 'Software + CAD', bio: "Lorem ipsum doler sait", mentor: false },
+    { name: 'Sebastian', role: 'CAD + Mechanical', bio: "Lorem ipsum doler sait", mentor: false },
+    { name: 'Emelia', role: 'Portfolio + Software', bio: "Lorem ipsum doler sait", mentor: false },
+    { name: 'Stephan', role: 'Portfolio', bio: "Lorem ipsum doler sait", mentor: false },
+    { name: 'Jayden', role: 'Portfolio', bio: "Lorem ipsum doler sait", mentor: false },
+    { name: 'Raymond', role: 'CAD + Mechanical', bio: "Lorem ipsum doler sait", mentor: false },
+    { name: 'Joe Ricker', role: 'Mentor + Team Admin', bio: "Lorem ipsum doler sait", mentor: true },
+    { name: 'Alison Ricker', role: 'Team Admin', bio: "Lorem ipsum doler sait", mentor: true },
 ];
 
 const Members = () => {
@@ -13,7 +18,7 @@ const Members = () => {
                 <div key={index} className="w-full aspect-video border-2 p-4">
                     <div className="flex flex-row gap-4">
                         <h1 className="text-xl">{member.name}</h1>
-                        <h1 className="text-lg text-zinc-900 bg-white px-2">{member.role}</h1>
+                        <h1 className={`text-lg text-zinc-900 px-2 ${member.mentor ? 'bg-blue-500' : 'bg-white'}`}>{member.role}</h1>
                     </div>
                     <p>{member.bio}</p>
                 </div>
