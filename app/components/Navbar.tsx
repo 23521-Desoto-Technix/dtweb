38897c5f5ca857325bdd3dfceb613c4fb8d6de37
PythonAtSea <HudsonGreen27@gmail.com>
2024-11-09 17:30:35 -0600

    mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full p-4 backdrop-blur-sm sticky top-0 border-b-2 border-white border-bottom">
+      <div className="w-full p-4 backdrop-blur-sm relative md:sticky top-0 border-b-2 border-white border-bottom">
         <div className="container mx-auto h-full">
-          <div className="flex justify-between items-center h-full">
-            <div className="flex text-white md:flex-row flex-col block">
+          <div className="flex justify-between items-center h-full flex-col md:flex-row">
+            <div className="flex text-white md:flex-row flex-col block justify-items-center md:justify-items-start">
               <Link href="/">
                 <p className="transition ease-in-out hover:-translate-y-1 duration-200 py-2 px-3 md:pl-0">DESOTO TECHNIX</p>
               </Link>
@@ -23,7 +23,7 @@ const Navbar = () => {
             </div>
             <button 
               onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })} 
-              className="transition ease-in-out hover:bg-white duration-200 text-white hover:text-zinc-900 border-2 border-white py-2 px-3 justify-self-end"
+              className="transition ease-in-out hover:bg-white duration-200 text-white hover:text-zinc-900 border-2 border-white py-2 px-3 justify-self-end w-full md:w-auto"
             >
               CONTACT US
             </button>
