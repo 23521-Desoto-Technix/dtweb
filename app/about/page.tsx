0266af9474f6004eb59176b5284501e50ee6f607
PythonAtSea <HudsonGreen27@gmail.com>
2024-11-09 20:03:03 -0600

    feat(About): add About and Not Found pages with team information

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Members from '../components/Members'
+
+const AboutPage = () => {
+    return (
+        <div className='py-10'>
+            <h1 className="text-4xl font-bold mb-4">About Us</h1>
+            <p className="mb-2">We are a community 2nd year <i>FIRST</i> Tech Challenge robotics team. Our team is composed of 7 students who are excited about robotics and technology. Last year, we qualified for the <i>FIRST</i> Championship and we aim to do so again this year.</p>
+            <h1 className="text-4xl py-10 pb-5">Team Members</h1>
+            <Members />
+        </div>
+    );
+};
+
+export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Sponsors from "./components/Sponsors";
 import Members from "./components/Members";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <div className="pr-6 md:pr-0 box-content">
         <h1 className="text-6xl pt-20 pb-10">DeSoto Technix</h1>
         <p className="pb-10">DeSoto Technix is a team dedicated to creating competitive robots for the <i>FIRST</i> Tech Challenge, as well as sharing our love of STEM with our community.</p>
-        <button className="transition ease-in-out hover:bg-white duration-200 text-white hover:text-zinc-900 border-2 border-white py-2 px-3">LEARN MORE</button>
+        <Link href="/about" className="transition ease-in-out hover:bg-white duration-200 text-white hover:text-zinc-900 border-2 border-white py-2 px-3">LEARN MORE</Link>
         <h1 className="text-4xl py-10 pb-5">Sponsors</h1>
         <p className="pb-10">Sponsors are crucial for our team as they provide the necessary funding for equipment, materials, and competition fees. Additionally, sponsors offer mentorship and industry connections that help our members gain valuable skills and experiences.</p>
         <Sponsors />
@@ -21,8 +22,7 @@ export default function Home() {
         >
           CONTACT US
         </button>
-        <h1 className="text-4xl py-10 pb-5">Team Members</h1>
-        <Members />
+        
       </div>
     </div>
   );
