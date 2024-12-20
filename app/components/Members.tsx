ad96178b99c8e617bf1091d2095a583dd5d648ff
PythonAtSea <HudsonGreen27@gmail.com>
2024-12-20 11:39:23 -0600

    fix(Members): update member roles and bios for clarity and accuracy

--- app/components/Members.tsx
+++ app/components/Members.tsx
@@ -1,12 +1,12 @@
 
 const members = [
     { name: 'Nathan', role: 'Captain', bio: "Nathan is a junior in high school and is the team captain and design lead.", mentor: false },
-    { name: 'Hudson', role: 'Software + CAD', bio: "Hudson is a sophomore in high school and is the software lead as well as being on the design team.", mentor: false },
-    { name: 'Sebastian', role: 'CAD + Mechanical', bio: "Sebastian is in 7th grade and is on the design and assembly teams.", mentor: false },
+    { name: 'Hudson', role: 'Software + Design', bio: "Hudson is a sophomore in high school and is the software lead as well as being on the design team.", mentor: false },
+    { name: 'Sebastian', role: 'Design + Build', bio: "Sebastian is in 7th grade and is on the design and build teams.", mentor: false },
     { name: 'Emelia', role: 'Portfolio + Software', bio: "Emelia is a sophomore in high school and is the portfolio lead as well as being on the software team.", mentor: false },
     { name: 'Stephan', role: 'Portfolio', bio: "Stephan is a sophomore in high school and is on the portfolio team.", mentor: false },
-    { name: 'Jayden', role: 'Portfolio + Mechanical', bio: "Jayden is a sophomore in high school and is on the portfolio and assembly teams.", mentor: false },
-    { name: 'Raymond', role: 'CAD + Mechanical', bio: "Raymond is a sophomore in high school and is on the portfolio and assembly teams.", mentor: false },
+    { name: 'Jayden', role: 'Portfolio + Build', bio: "Jayden is a sophomore in high school and is on the portfolio and build teams.", mentor: false },
+    { name: 'Raymond', role: 'Design + Mechanical', bio: "Raymond is a sophomore in high school and is on the design and build teams.", mentor: false },
     { name: 'Joe Ricker', role: 'Mentor + Coach', bio: "Mr. Joe is the team mentor and coach", mentor: true },
     { name: 'Alison Ricker', role: 'Team Admin', bio: "Ms. Alison is the assistant coach and team administrator", mentor: true },
 ];
