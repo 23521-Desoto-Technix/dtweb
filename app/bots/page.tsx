f80519fc895212e308c46e01636a3f7d361c2906
PythonAtSea <HudsonGreen27@gmail.com>
2025-03-27 18:09:44 -0500

    feat(Robots): update robot details and images for Depth Charge v1 and v2

--- app/bots/page.tsx
+++ app/bots/page.tsx
@@ -3,14 +3,21 @@ import Image from 'next/image';
 import Button from '../components/Button';
 import CS from '../../public/IMG_5497.jpg';
 import off24 from '../../public/IMG_5498.jpg';
-import ITD from '../../public/FullSizeRender.jpg';
+import ITDv1 from '../../public/FullSizeRender.jpg';
+import ITDv2 from '../../public/mschamps03.jpg'
 
 const RobotsPage = () => {
     const robots = [
         {
-            name: 'Depth Charge',
-            image: ITD,
-            description: 'Depth Charge is our robot for the 2024-2025 INTO THE DEEP game, and was completely designed in CAD before it was built.',
+            name: 'Depth Charge v2',
+            image: ITDv2,
+            description: 'Depth Charge v2 is our second robot for INTO THE DEEP, and has numerous improvements over v1, including better packaging, improved specimen scoring, and more accurate odometry.',
+            link: ""
+        },
+        {
+            name: 'Depth Charge v1',
+            image: ITDv1,
+            description: 'Depth Charge v1 is our first robot for the 2024-2025 INTO THE DEEP game, and was completely designed in CAD before it was built.',
             link: ""
         },
         {
@@ -34,7 +41,7 @@ const RobotsPage = () => {
             <p className='py-5'>For more info about our robots, you can contact our design lead at <a href="mailto:nathanricker44@gmail.com" className='hover:underline text-blue-500'>nathanricker44@gmail.com</a>, or DM @dragonhero1051 on discord.</p>
             <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
                 {robots.map((robot, index) => (
-                    <div key={index} className="border-2 border-gray-300 shadow-md p-4 pb-6">
+                    <div key={index} className="border-2 border-white shadow-md p-4 pb-6">
                         <h2 className="text-xl font-semibold mb-2">{robot.name}</h2>
                         <Image src={robot.image} width={300} height={200} className="mb-2 w-full border-2 border-white p-1" alt={robot.name} />
                         <p className="">{robot.description}</p>
